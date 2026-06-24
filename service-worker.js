e7154cadace2396afa76b763f971f170b153b667
PIGEON SINGAPORE <veronica.poo@pigeon.com>
2026-06-24 08:37:02 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const CACHE = 'mikuron-wo-v1';
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => self.clients.claim());
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+      return resp;
+    }).catch(() => caches.match(e.request))
+  );
+});
